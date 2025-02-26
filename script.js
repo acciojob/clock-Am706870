943bb53fb323f4922304392368c35f4ee4ba50b3
Am706870 <88397078+Am706870@users.noreply.github.com>
2025-02-27 00:11:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function updateTimer() {
+	let newTimer = document.getElementById("timer");
+	const currentDate = new Date();
+	let setTimer = currentDate.toLocaleString();
+
+	newTimer.textContent = setTimer;
+}
+setInterval(updateTimer(),1000);
+updateTimer();
